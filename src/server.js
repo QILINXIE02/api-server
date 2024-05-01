43213a785803fe91fc15a4ef0561c4370ceb549f
qilinxie02 <qxie@gicw.org>
2024-05-01 13:35:51 -0700

    set up for basic API

---
+++ src/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const notFoundHandler = require('./handlers/404.js');
+const errorHandler = require('./handlers/500.js');
+
+const peopleRoutes = require('./routes/people.js');
+
+app.use(cors());
+app.use(express.json());
+
+app.use(peopleRoutes);
+
+app.get('/broken', (req,res,next) => next("whoops!"));
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+function start(port) {
+    app.listen(port, () => {
+        console.log(`Server is up on ${port}`);
+    });
+}
+
+module.exports = { app, start };
